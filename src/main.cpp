fcdc447014532fea374bdf53131fa67fb30d29df
Tsundra <tsundradev@gmail.com>
2024-09-21 09:10:40 +0700

    LogLevel implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 
+#include <tools/Log.hpp>
+
 int main() {
 
   printf("c-log\n");
---
+++ src/tools/Log/LogLevel.hpp
@@ -0,0 +1,20 @@
+#ifndef __TOOLS__LOG__LOGLEVEL_HPP__
+#define __TOOLS__LOG__LOGLEVEL_HPP__
+
+// -------------------------------------------------------------------------------------------------
+// -- LogLevel enum
+// -------------------------------------------------------------------------------------------------
+class LogLevel {
+public:
+  enum Enum {FATAL, ERROR, WARNING, INFO, DEBUG};
+
+  LogLevel(Enum e) : m_e(e) {}
+  operator Enum() const { return m_e; }
+  explicit operator bool() const = delete;
+
+
+private:
+  Enum m_e;
+};
+
+#endif
